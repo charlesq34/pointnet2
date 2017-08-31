cfffde1a9343bee97e518f2436839df827fd61b2
Charles R. Qi <rqi@nuro.ai>
2017-08-31 13:40:51 -0700

    make layer names consistent

--- models/pointnet2_cls_msg.py
+++ models/pointnet2_cls_msg.py
@@ -20,16 +20,16 @@ def get_model(point_cloud, is_training, bn_decay=None):
     num_point = point_cloud.get_shape()[1].value
     end_points = {}
 
-    xyz0 = point_cloud
-    points0 = None
+    l0_xyz = point_cloud
+    l0_points = None
 
     # Set abstraction layers
-    xyz1, points1 = pointnet_sa_module_msg(xyz0, points0, 512, [0.1,0.2,0.4], [32,64,128], [[32,32,64], [64,64,128], [64,96,128]], is_training, bn_decay, scope='layer1')
-    xyz2, points2 = pointnet_sa_module_msg(xyz1, points1, 128, [0.2,0.4,0.8], [64,64,128], [[64,64,128], [128,128,256], [128,128,256]], is_training, bn_decay, scope='layer2')
-    xyz3, points3, _ = pointnet_sa_module(xyz2, points2, npoint=None, radius=None, nsample=None, mlp=[256,512,1024], mlp2=None, group_all=True, is_training=is_training, bn_decay=bn_decay, scope='layer3')
+    l1_xyz, l1_points = pointnet_sa_module_msg(l0_xyz, l0_points, 512, [0.1,0.2,0.4], [32,64,128], [[32,32,64], [64,64,128], [64,96,128]], is_training, bn_decay, scope='layer1')
+    l2_xyz, l2_points = pointnet_sa_module_msg(l1_xyz, l1_points, 128, [0.2,0.4,0.8], [64,64,128], [[64,64,128], [128,128,256], [128,128,256]], is_training, bn_decay, scope='layer2')
+    l3_xyz, l3_points, _ = pointnet_sa_module(l2_xyz, l2_points, npoint=None, radius=None, nsample=None, mlp=[256,512,1024], mlp2=None, group_all=True, is_training=is_training, bn_decay=bn_decay, scope='layer3')
 
     # Fully connected layers
-    net = tf.reshape(points3, [batch_size, -1])
+    net = tf.reshape(l3_points, [batch_size, -1])
     net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training, scope='fc1', bn_decay=bn_decay)
     net = tf_util.dropout(net, keep_prob=0.4, is_training=is_training, scope='dp1')
     net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training, scope='fc2', bn_decay=bn_decay)
