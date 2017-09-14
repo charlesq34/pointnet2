910ebd2781e1628fcd78c3f28b8171c785ee5749
Charles R. Qi <charlesq34@gmail.com>
2017-09-13 20:57:11 -0700

    Update pointnet_util.py

--- utils/pointnet_util.py
+++ utils/pointnet_util.py
@@ -6,7 +6,7 @@ Date: November 2017
 
 import os
 import sys
-BASE_DIR = os.path.dirname(__file__)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(BASE_DIR)
 sys.path.append(os.path.join(ROOT_DIR, 'utils'))
 sys.path.append(os.path.join(ROOT_DIR, 'tf_ops/sampling'))
