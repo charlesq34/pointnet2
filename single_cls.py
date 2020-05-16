0e3ab7064347691089f221976a4d7684426ede06
irfan798 <irfanbilaloglu@gmail.com>
2020-05-16 20:57:06 +0300

    Predict class of single pointcloud data

--- single_cls.py
+++ single_cls.py
@@ -1,50 +1,42 @@
 '''
-    Evaluate classification performance with optional voting.
-    Will use H5 dataset in default. If using normal, will shift to the normal dataset.
+    Predict class of single pointcloud data.
 '''
 import tensorflow as tf
 import numpy as np
 import argparse
-import socket
 import importlib
-import time
 import os
-import scipy.misc
 import sys
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = BASE_DIR
 sys.path.append(BASE_DIR)
 sys.path.append(os.path.join(ROOT_DIR, 'models'))
 sys.path.append(os.path.join(ROOT_DIR, 'utils'))
+
 import provider
-import modelnet_dataset
-import modelnet_h5_dataset
 
 from pprint import pprint
-
-from show3d_balls import showpoints
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--gpu', type=int, default=0, help='GPU to use [default: GPU 0]')
 parser.add_argument('--model', default='pointnet2_cls_ssg', help='Model name. [default: pointnet2_cls_ssg]')
-parser.add_argument('--batch_size', type=int, default=16, help='Batch Size during training [default: 16]')
 parser.add_argument('--num_point', type=int, default=1024, help='Point Number [256/512/1024/2048] [default: 1024]')
 parser.add_argument('--model_path', default='log/model.ckpt', help='model checkpoint file path [default: log/model.ckpt]')
 parser.add_argument('--dump_dir', default='dump', help='dump folder path [dump]')
 parser.add_argument('--normal', action='store_true', help='Whether to use normal information')
 parser.add_argument('--num_votes', type=int, default=1, help='Aggregate classification scores from multiple rotations [default: 1]')
-
+parser.add_argument('-v', '--visualize', action='store_true', help='Visualize input pointcloud data')
 parser.add_argument('--path', help='Path of pointcloud txt')
 
 FLAGS = parser.parse_args()
-FLAGS.normal = True
 
-BATCH_SIZE = 1
 NUM_POINT = FLAGS.num_point
 MODEL_PATH = FLAGS.model_path
 GPU_INDEX = FLAGS.gpu
 MODEL = importlib.import_module(FLAGS.model) # import network module
 DUMP_DIR = FLAGS.dump_dir
+VISUALIZE = FLAGS.visualize
 if not os.path.exists(DUMP_DIR): os.mkdir(DUMP_DIR)
 LOG_FOUT = open(os.path.join(DUMP_DIR, 'log_evaluate.txt'), 'w')
 LOG_FOUT.write(str(FLAGS)+'\n')
@@ -53,12 +45,10 @@ NUM_CLASSES = 40
 SHAPE_NAMES = [line.rstrip() for line in \
     open(os.path.join(ROOT_DIR, 'data/modelnet40_ply_hdf5_2048/shape_names.txt'))] 
 
-HOSTNAME = socket.gethostname()
 
-
-#pc_path = 'data/modelnet40_normal_resampled/bench/bench_0002.txt'
-pc_path = FLAGS.path
-npoints = 3
+PC_PATH = FLAGS.path
+# Get first n dimensions, must change with normal flag
+npoints = 3 
 
 
 def log_string(out_str):
@@ -66,11 +56,10 @@ def log_string(out_str):
     LOG_FOUT.flush()
     print(out_str)
 
-def evaluate(num_votes):
-    is_training = False
+def evaluate():
      
     with tf.device('/gpu:'+str(GPU_INDEX)):
-        pointclouds_pl, labels_pl = MODEL.placeholder_inputs(BATCH_SIZE, NUM_POINT)
+        pointclouds_pl, labels_pl = MODEL.placeholder_inputs(1, NUM_POINT)
         is_training_pl = tf.placeholder(tf.bool, shape=())
 
         # simple model
@@ -96,37 +85,48 @@ def evaluate(num_votes):
            'pred': pred
     }
 
-    point_set = np.loadtxt(pc_path, delimiter=',').astype(np.float32)
-    random_idx = np.random.randint(point_set.shape[0], size=1024)
-    print(point_set.shape)
+    # Load pointcloud data from txt file
+    point_set = np.loadtxt(PC_PATH, delimiter=',').astype(np.float32)
+    # Get indexes for random points from pointcloud
+    random_idx = np.random.randint(point_set.shape[0], size=1024) 
 
     #point_set = point_set[random_idx,0:npoints]
     point_set = point_set[:NUM_POINT, 0:npoints]
+    point_set = np.array([point_set])
 
-
-    #eval_one_epoch(sess, ops, num_votes)
-    pred_one(sess, ops, [point_set])
+    pred_one(sess, ops, point_set)
 
 def pred_one(sess, ops, pointcloud_data):
     is_training = False
+    num_votes = FLAGS.num_votes
 
-    #rotated_data = provider.rotate_point_cloud_by_angle(cur_batch_data[:, shuffled_indices, :], vote_idx/float(num_votes) * np.pi * 2)
-    feed_dict = {ops['pointclouds_pl']: pointcloud_data,
-                    ops['is_training_pl']: is_training}
+    pred_val_sum = np.zeros((1, NUM_CLASSES))
 
-    pred_val = sess.run([ops['pred']], feed_dict=feed_dict)
+    for vote_idx in range(num_votes):
 
-    print(pred_val)
-    idx = np.argmax(pred_val)
-    print(idx)
-    print(SHAPE_NAMES[idx])
+        rotation = vote_idx/float(num_votes) * np.pi * 2
+        rotated_data = provider.rotate_point_cloud_by_angle(pointcloud_data, rotation)
 
-    showpoints(pointcloud_data[0])
+        feed_dict = {ops['pointclouds_pl']: rotated_data,
+                        ops['is_training_pl']: is_training}
 
+        pred_val = sess.run([ops['pred']], feed_dict=feed_dict)[0]
+        pred_val_sum += pred_val
+        idx = np.argmax(pred_val)
+
+        print("Predicted shape as: '{}' with rotation: {}".format(SHAPE_NAMES[idx], np.degrees(rotation)) )
+    
+    final_idx = np.argmax(pred_val_sum)
+    print("Final prediction:", SHAPE_NAMES[final_idx])
+
+    if VISUALIZE:
+        from show3d_balls import showpoints
+
+        showpoints(pointcloud_data[0])
 
 
 if __name__=='__main__':
     with tf.Graph().as_default():
-        evaluate(num_votes=FLAGS.num_votes)
+        evaluate()
     LOG_FOUT.close()
 
